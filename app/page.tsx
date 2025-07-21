6991f12a4cf6e271b3ac5bd29ee7849ebd101fde
Lee Min Woo <lmw7414@naver.com>
2025-07-21 17:58:48 +0900

    #41- feat: 루트페이지, 공통 컴포넌트, 커스텀 훅 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import {Footer} from "@/components/layout/footer"
+import {Header} from "@/components/layout/header"
+
 export default function Home() {
   return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+    <Header />
     <main>
-      <h1> SIMPAYLOG 프로젝트입니다!</h1>
+      <h1>SIMPAYLOG 프로젝트입니다!</h1>
     </main>
+    <Footer />
+    </div>
   );
 }
