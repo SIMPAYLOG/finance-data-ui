a7e43f05b642cb6a0cf8d14424bd99a0c67663d7
Lee Min Woo <lmw7414@naver.com>
2025-07-22 10:07:01 +0900

    #41 - 메인 페이지 작업
    서비스의 내용을 대략적으로 나타내어 화면 구성

---
+++ app/generate/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import { BarChart3, ArrowLeft, ArrowRight } from "lucide-react"
+
+
+
+export default function GeneratePage() {
+
+  return (
+    <>
+    데이터 생성하는 페이지입니다!
+    </>
+  );
+}
